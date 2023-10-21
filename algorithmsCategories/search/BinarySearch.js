71c1861d2417c060b419158f245931d7cb0e8957
zahra <zahra0a1418@gmail.com>
2023-10-21 14:58:27 +0330

    add iterative binary search

--- algorithmsCategories/search/BinarySearch.js
+++ algorithmsCategories/search/BinarySearch.js
@@ -1,5 +1,5 @@
 export const recBinarySearch = (array, x) => {
-    const midIndex = Math.floor(array.length / 2)
+    const midIndex = Math.floor((array.length - 1) / 2)
     const midValue = array[midIndex];
     if (midValue === x) return midValue;
     if (midValue > x) {
@@ -7,4 +7,22 @@ export const recBinarySearch = (array, x) => {
     } else {
         return recBinarySearch(array.slice(midIndex + 1), x)
     }
+}
+
+export const iterativeBinarySearch = (array, x) => {
+    let copyArray = [...array];
+    while (copyArray.length >= 1) {
+        const midIndex = Math.floor((copyArray.length - 1) / 2);
+        const midValue = copyArray[midIndex];
+        if (midValue === x) {
+            return midValue;
+        }
+
+        if (midValue < x) {
+            copyArray = copyArray.slice(midIndex + 1);
+        } else {
+            copyArray = copyArray.slice(0, midIndex);
+        }
+    }
+
 }
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import { recBinarySearch } from "./algorithmsCategories/search/BinarySearch";
+import { recBinarySearch, iterativeBinarySearch } from "./algorithmsCategories/search/BinarySearch";
 
 const binaryArray = [10, 20, 30, 40, 50, 60];
 console.log(recBinarySearch(binaryArray, 50));
+console.log(iterativeBinarySearch(binaryArray, 50));
