12bf1d4709247135f8c0fb0dbdc8701c2292c4a4
zahra <zahra0a1418@gmail.com>
2023-10-29 07:15:16 +0330

    add open address  method for collision

--- algorithmsCategories/search/HashTable.js
+++ algorithmsCategories/search/HashTable.js
@@ -17,6 +17,8 @@ export class Cryptographic {
             const address = this.findAddress(key);
             if (this.method === collisionMethod.CHAINING) {
                 this.chaining(address, key);
+            } else {
+                this.openAddress(address, key);
             }
         }
     }
@@ -30,6 +32,32 @@ export class Cryptographic {
             this.hashTable[address] = [key];
         } else {
             this.hashTable[address].unshift(key)
+        }
+    }
+
+    openAddress = (address, key) => {
+        if (!this.hashTable[address]) {
+            this.hashTable[address] = key;
+        } else {
+            let flag = false;
+
+            for (let i = address + 1; i < this.hashTable.length; i++) {
+                if (!this.hashTable[i]) {
+                    this.hashTable[i] = key;
+                    flag = true;
+                    return;
+                }
+            }
+
+            if (!flag) {
+                for (let i = 0; i < address; i++) {
+                    if (!this.hashTable[i]) {
+                        this.hashTable[i] = key;
+                        return;
+                    }
+                }
+            }
+
         }
     }
 }
