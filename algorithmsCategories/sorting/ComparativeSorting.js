708ad039c346b5301f60559c368916efbab5f991
zahra <zahra0a1418@gmail.com>
2023-11-02 12:53:25 +0330

    add an exit condition to the sort function

--- algorithmsCategories/sorting/ComparativeSorting.js
+++ algorithmsCategories/sorting/ComparativeSorting.js
@@ -6,10 +6,16 @@ const swap = (array, a, b) => {
 
 export const BubbleSort = (array) => {
     for (let i = 0; i < array.length - 1; i++) {
+        let swapped = false;
         for (let j = i + 1; j < array.length; j++) {
             if (array[j] < array[i]) {
                 swap(array, i, j);
+                swapped = true;
             }
+        }
+
+        if (!swapped) {
+            return;
         }
     }
 
