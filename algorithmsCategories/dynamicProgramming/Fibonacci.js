382a6e642e325c99c648ba8e6dfd3d22eb2b10fa
zahra <zahra0a1418@gmail.com>
2023-11-14 19:00:17 +0330

    add multiplytwo matrix

--- algorithmsCategories/dynamicProgramming/Fibonacci.js
+++ algorithmsCategories/dynamicProgramming/Fibonacci.js
@@ -16,6 +16,27 @@ export const fibonancciDynamicProgramming = (n) => {
     return f;
 }
 
+const multiplyMatrix = (matrix1, matrix2) => {
+    const rows1 = matrix1.length;
+    const cols1 = matrix1[0].length;
+    const rows2 = matrix2.length;
+    const cols2 = matrix2[0].length;
+    const result = new Array(rows1).fill().map(() => new Array(cols2).fill(0));
+
+    if (cols1 !== rows2) {
+        throw new Error("Invalid matrixs");
+    }
+
+    for (let i = 0; i < rows1; i++) {
+        for (let j = 0; j < cols1; j++) {
+            for (let k = 0; k < cols2; k++) {
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+    return result;
+}
+
 const powerMatrix = (baseMatrix, n) => {
 
 }
