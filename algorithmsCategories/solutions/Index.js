4b3d6b09457ee2f4ee3063fd2e02e4f6b5bf0d7b
zahra <zahra0a1418@gmail.com>
2024-01-01 10:22:26 +0330

    add jumping solution

--- algorithmsCategories/solutions/Index.js
+++ algorithmsCategories/solutions/Index.js
@@ -1,3 +1,9 @@
+
+// You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+// Each element nums[i] represents the maximum length of a forward jump from index i.
+// In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+// return minimum jump to receive n-1 element
+
 export const jump1 = (nums) => {
     const n = nums.length;
     let index = 0;
